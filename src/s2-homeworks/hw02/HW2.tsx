884b763c8adc114241ac145a0c8feb15a9e496f4
Vitalik <05vp94@mail.ru>
2022-10-21 13:53:55 +0300

    task 2 done

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -62,6 +62,7 @@ function HW2() {
     const [affairs, setAffairs] = useState<Array<AffairType>>(defaultAffairs) // need to fix any
     const [filter, setFilter] = useState<FilterType>('all')
 
+
     const filteredAffairs = filterAffairs(affairs, filter)
 
     const deleteAffairCallback = (_id: number) => { // need to fix any
