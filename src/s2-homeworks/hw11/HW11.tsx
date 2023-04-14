3422d2a6458411747d30a60478800581119130f0
Vitali.Palcheuski <05vp94@mail.ru>
2023-04-14 16:35:58 +0300

    homework 11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -13,10 +13,22 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+    const [value2, setValue2] = useState(restoreState<number[]>('hw11-value2', [0,100]))
 
-    const change = (event: any, value: any) => {
+    const change = (event: Event, value: number | number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+
+        if(Array.isArray(value)){
+            console.log(value)
+            setValue1(value[0])
+            setValue2(value)
+
+        } else {
+            setValue1(value as number)
+            setValue2([value, value2[1]])
+        }
+
+
     }
 
     return (
@@ -30,7 +42,9 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
-
+                            value={value1}
+                            onChange={change}
+                            valueLabelDisplay="auto"
                         />
                     </div>
                     <div className={s.wrapper}>
@@ -38,7 +52,9 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
+                            value={value2}
+                            onChange={change}
+                            valueLabelDisplay="auto"
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
